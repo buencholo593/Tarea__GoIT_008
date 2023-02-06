7ec62f74c1999fa0c7934bcd4ee8ed7c435f90a2
buencholo <david_arq.violin@yahoo.com>
2023-02-06 11:45:07 -0500

    coding JS

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const divRef = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+return items.map(
+    (item) => `<div class="gallery__item">
+                    <a class="gallery__link" href="${item.original}">
+                    <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                    </a>
+                    </div>`
+)
+.join("")
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+divRef.innerHTML = addGalleryMarkup;
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250,
+    captionData: "alt"
+})
