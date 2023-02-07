82f2946bcc2feda088cb428b2bbf1e09b6cc82ce
buencholo <david_arq.violin@yahoo.com>
2023-02-07 13:22:12 -0500

    Player without bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function(tittle) {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    console.log(data)
+    // data is an object containing properties specific to that event
+};
+player.on('timeupdate', onPlay);
